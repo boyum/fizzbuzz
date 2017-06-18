e2a71f4b0f196f36a49f9c5740e656bf03b7996a
Sindre <sindre.boyum@gmail.com>
2017-06-18 14:45:09 +0200

    ADD: Added XO (https://github.com/sindresorhus/xo) linter, fixed a few issues it found

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,26 +1,28 @@
 /* global console */
 
 function fizzbuzz() {
-    const n = 100;
+  const n = 100;
 
-    Array.apply(0, Array(n)).map((el, i) => {
-        console.log(getFizzOrBuzz(i+1));
-    });    
+  const fizzBuzzArr = Array.apply(0, Array(n)).map((el, i) => {
+    return getFizzOrBuzz(i + 1);
+  });
+
+  console.log(fizzBuzzArr);
 }
 
 /**
- * Returns 'fizz' if the number is divisable by 3, 
- * 'buzz' if the number is divisable by 5, 
+ * Returns 'fizz' if the number is divisable by 3,
+ * 'buzz' if the number is divisable by 5,
  * 'fizzbuzz' if dividable by 15,
  * and the number otherwise
- * @param {number} num 
+ * @param {number} num
  * @return {String|Number}
  */
 function getFizzOrBuzz(num) {
-    const f = !(num % 3);
-    const b = !(num % 5);
+  const f = !(num % 3);
+  const b = !(num % 5);
 
-    return f | b ? f ? b ? 'Fizzbuzz' : 'Fizz' : 'Buzz' : num;
+  return f | b ? f ? b ? 'Fizzbuzz' : 'Fizz' : 'Buzz' : num;
 }
 
 fizzbuzz();
