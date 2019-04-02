99f8111deeb66fae0f29c8ae1e58835f64b91f6b
Sindre Bøyum <sindre@acos.no>
2019-04-02 19:08:16 +0200

    Updated ava+babel

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -2,7 +2,7 @@ function fizzbuzz() {
   const n = 100;
 
   const fizzBuzzArr = Array
-    .apply(0, Array(n))
+    .apply(0, new Array(n))
     .map((el, i) => getFizzOrBuzz(i + 1));
 
   console.log(fizzBuzzArr);
@@ -13,8 +13,8 @@ function fizzbuzz() {
  * 'buzz' if the number is divisable by 5,
  * 'fizzbuzz' if divisable by 15,
  * and the number otherwise
- * @param {number} num
- * @return {string|number}
+ * @param {number} num number to check
+ * @return {string|number} 'fizz' if num is divisable by 3, 'buzz' if num is divisable by 5, 'fizzbuzz' if num is divisable by 15, or num's value
  */
 function getFizzOrBuzz(num) {
   const f = !(num % 3);
