3f0ebdde22211739b8921c3557b4e6efefba07a9
Sindre Bøyum <sindre.boyum@gmail.com>
2020-07-07 13:04:13 +0200

    Update deps

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,11 +1,11 @@
 function fizzbuzz() {
   const n = 100;
 
-  const fizzBuzzArr = Array
-    .apply(0, new Array(n))
-    .map((el, i) => getFizzOrBuzz(i + 1));
+  const fizzBuzzArray = new Array(n)
+    .fill(0)
+    .map((_, i) => [i + 1, getFizzOrBuzz(i + 1)]);
 
-  console.log(fizzBuzzArr);
+  console.table(fizzBuzzArray);
 }
 
 /**
@@ -13,14 +13,15 @@ function fizzbuzz() {
  * 'buzz' if the number is divisable by 5,
  * 'fizzbuzz' if divisable by 15,
  * and the number otherwise
- * @param {number} num number to check
+ * @param {number} number number to check
  * @return {string|number} 'fizz' if num is divisable by 3, 'buzz' if num is divisable by 5, 'fizzbuzz' if num is divisable by 15, or num's value
  */
-function getFizzOrBuzz(num) {
-  const f = !(num % 3);
-  const b = !(num % 5);
+function getFizzOrBuzz(number) {
+  const f = !(number % 3);
+  const b = !(number % 5);
 
-  return f | b ? f ? b ? 'Fizzbuzz' : 'Fizz' : 'Buzz' : num;
+  // eslint-disable-next-line unicorn/no-nested-ternary
+  return f | b ? f ? b ? 'Fizzbuzz' : 'Fizz' : 'Buzz' : number;
 }
 
 fizzbuzz();
--- js/fizzbuzz.spec.js
+++ js/fizzbuzz.spec.js
@@ -3,17 +3,17 @@ import test from 'ava';
 import {getFizzOrBuzz} from './fizzbuzz';
 
 test('return \'Fizz\' if divisable by 3', t => {
-  t.deepEqual(getFizzOrBuzz(36), 'Fizz');
+  t.is(getFizzOrBuzz(36), 'Fizz');
 });
 
 test('return \'Buzz\' if divisable by 5', t => {
-  t.deepEqual(getFizzOrBuzz(20), 'Buzz');
+  t.is(getFizzOrBuzz(20), 'Buzz');
 });
 
 test('return \'Fizzbuzz\' if divisable by 15', t => {
-  t.deepEqual(getFizzOrBuzz(30), 'Fizzbuzz');
+  t.is(getFizzOrBuzz(30), 'Fizzbuzz');
 });
 
 test('return the given number if not divisable by 3 or 5', t => {
-  t.deepEqual(getFizzOrBuzz(17), 17);
+  t.is(getFizzOrBuzz(17), 17);
 });
